027031de9c0cd5de0b5f9ad7a18a8064434ccd89
[Joyce 101 PC] Angus Goucher <gus.goucher@gmail.com>
2024-04-24 20:30:16 -0400

    bye bye love by simon and garfunkel

--- build/external/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gus/Projects/netgame/external/raylib/src
-# Build directory: /Users/gus/Projects/netgame/build/external/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
